f9ac773476db9e9734453434e9b486753459fd04
hi-haho <from_me_you@naver.com>
2024-11-04 17:20:10 +0900

    initial commit

---
+++ src/main/java/com/aendyear/komawatsir/KomawatsirApplication.java
@@ -0,0 +1,13 @@
+package com.aendyear.komawatsir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KomawatsirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KomawatsirApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aendyear/komawatsir/KomawatsirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aendyear.komawatsir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KomawatsirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

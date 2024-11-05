215259f24c820510640f65a3364f87054257b9bf
hyo <hyoseonee0729@naver.com>
2024-11-05 20:24:13 +0900

    hyo 2nd

--- src/main/java/com/aendyear/komawatsir/KomawatsirApplication.java
+++ src/main/java/com/aendyear/komawatsir/KomawatsirApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KomawatsirApplication { //ss
+public class KomawatsirApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KomawatsirApplication.class, args);

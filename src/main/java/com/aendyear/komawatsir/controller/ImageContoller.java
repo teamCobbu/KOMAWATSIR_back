0009242303f20be0f5ef367190165c1924a7f7c8
dazz6 <dazz6@kakao.com>
2024-11-16 14:53:49 +0900

    image 정보 조회 등

--- src/main/java/com/aendyear/komawatsir/controller/ImageContoller.java
+++ src/main/java/com/aendyear/komawatsir/controller/ImageContoller.java
@@ -1,9 +1,14 @@
 package com.aendyear.komawatsir.controller;
 
+import com.aendyear.komawatsir.dto.ImageDto;
 import com.aendyear.komawatsir.service.ImageService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/images")
@@ -11,18 +16,25 @@ public class ImageContoller {
 
     @Autowired
     private ImageService imageService;
-    // 이미지 추가
+
+    // todo : 이미지 서버 업로드 일단 모르는 척
 //    @PostMapping("/{type}")
+//    @Operation(summary = "add image", description = "이미지 업로드")
 
-    // 이미지 조회 (단일 이미지)
-//    @GetMapping("/{type}/{id}")
+    @GetMapping("/image/{imageId}")
+    @Operation(summary = "load single image", description = "단일 이미지 조회")
+    public ResponseEntity<ImageDto> getSingleImage(@PathVariable Integer imageId) {
+        return ResponseEntity.ok(imageService.getSingleImage(imageId));
+    }
 
+    // todo : 카테고리 + 페이징...
     // 이미지 목록 조회
 //    @GetMapping("/{type}")
 
-    // 이미지 카테고리 목록 조회
-//    @GetMapping("/{category}")
+    @GetMapping("/category/list")
+    @Operation(summary = "load category list", description = "이미지 카테고리 목록 조회")
+    public ResponseEntity<List<String>> getCategoryList() {
+        return ResponseEntity.ok(imageService.getCategoryList());
+    }
 
-    // 이미지 정보 수정
-//    @PutMapping("/{type}/{id}")
 }
--- src/main/java/com/aendyear/komawatsir/service/ImageService.java
+++ src/main/java/com/aendyear/komawatsir/service/ImageService.java
@@ -1,9 +1,39 @@
 package com.aendyear.komawatsir.service;
 
+import com.aendyear.komawatsir.dto.ImageDto;
+import com.aendyear.komawatsir.entity.Image;
+import com.aendyear.komawatsir.repository.ImageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @Validated
 public class ImageService {
+
+    @Autowired
+    private ImageRepository imageRepository;
+
+    // 이미지 정보 조회
+    public ImageDto getSingleImage(Integer imageId) {
+        ImageDto result = new ImageDto();
+
+        Optional<Image> image = imageRepository.findById(imageId);
+        if (image.isPresent()) {
+            result = Mapper.toDto(image.get());
+        }
+
+        return result;
+    }
+
+    // 카테고리 목록 조회
+    public List<String> getCategoryList() {
+        List<String> result = new ArrayList<>();
+        imageRepository.findAll().stream().map(Image::getCategory).distinct().forEach(result::add);
+        return result;
+    }
 }

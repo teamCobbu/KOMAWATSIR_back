3bae8d5988fa83ee77a1ef7a489d77cd7ec9878f
dazz6 <dazz6@kakao.com>
2024-11-17 21:03:51 +0900

    url (hmac)

--- src/main/java/com/aendyear/komawatsir/service/InquiryService.java
+++ src/main/java/com/aendyear/komawatsir/service/InquiryService.java
@@ -5,13 +5,18 @@ import com.aendyear.komawatsir.entity.Inquiry;
 import com.aendyear.komawatsir.entity.InquiryItem;
 import com.aendyear.komawatsir.repository.InquiryItemRepository;
 import com.aendyear.komawatsir.repository.InquiryRepository;
+import jakarta.annotation.PostConstruct;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -28,6 +33,16 @@ public class InquiryService {
 
     @Autowired
     private InquiryItemRepository inquiryItemRepository;
+
+    @Value("${komawatsir-pwd}")
+    private String secretKey;
+
+    private static String SECRET_KEY = "";
+
+    @PostConstruct
+    public void init() {
+        SECRET_KEY = this.secretKey;
+    }
 
     // 신청 질문 목록 불러오기
     public List<InquiryItemDto> getQuestionList(Integer userId) {
@@ -82,5 +97,37 @@ public class InquiryService {
         inquiryItemRepository.deleteById(deleteId);
 
         return deleteId;
+    }
+
+    // url 추출하기
+    public String getUrl(Integer userId) {
+        try {
+            SecretKeySpec secretKey = new SecretKeySpec(SECRET_KEY.getBytes(), "HmacSHA256");
+            Mac mac = Mac.getInstance("HmacSHA256");
+            mac.init(secretKey);
+
+            byte[] hmacBytes = mac.doFinal(String.valueOf(userId).getBytes());
+
+            return Base64.getUrlEncoder().withoutPadding().encodeToString(hmacBytes);
+        } catch (Exception e) {
+            System.out.println("getUrl ERROR : " + e.getMessage());
+            return null;
+        }
+    }
+
+    // url 검증하기
+    public Boolean validateUrl(Integer userId, String url) {
+        String userIdHmac = getUrl(userId);
+
+       if (userIdHmac == null || url == null || userIdHmac.length() != url.length()) {
+           return false;
+       }
+
+       int result = 0;
+       for (int i = 0; i < userIdHmac.length(); i++) {
+           result |= userIdHmac.charAt(i) ^ url.charAt(i);
+       }
+
+        return result == 0;
     }
 }

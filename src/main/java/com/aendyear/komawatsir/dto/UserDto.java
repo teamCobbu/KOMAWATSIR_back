0b198398ee88d73efdee8a18a92e1012e0d8385e
hi-haho <from_me_you@naver.com>
2024-12-14 15:30:05 +0900

    validation tel 010,011 시작으로 변경

--- src/main/java/com/aendyear/komawatsir/dto/UserDto.java
+++ src/main/java/com/aendyear/komawatsir/dto/UserDto.java
@@ -15,7 +15,7 @@ public class UserDto {
     private Integer id;
     @Pattern(regexp = "^[A-Za-z가-힣]+$")
     private String name;
-    @Pattern(regexp = "^[0-9]{11}$", message = "전화번호는 11자리 숫자만 가능합니다.")
+    @Pattern(regexp = "^(010|011)[0-9]{8}$", message = "전화번호는 11자리 숫자만 가능합니다.")
     private String tel;
     @Pattern(regexp = "^[0-9]{10,11}$")
     private String kakaoId;
